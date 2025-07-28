b29c29f12401f7a903565f355f6c4987d426dc54
yx <48951196+bngybongybrd@users.noreply.github.com>
2025-07-28 05:24:08 +0000

    Exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, count) {
+    res = ""
+    if (count < 0) {
+        return "ERROR"
+    }
 
+    for (let i = 0; i < count; i++) {
+        res += str
+    }
+    return res
 };
 
 // Do not edit below this line
