2c88a99585999be4f517bbe896c220a378b510ac
yx <48951196+bngybongybrd@users.noreply.github.com>
2025-08-10 21:20:52 +0800

    completed exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = [...str]
+        .filter(chr => /^[a-z0-9]+$/i.test(chr))
+        .map(s => s.toLowerCase())
+    let revarr = arr.slice().reverse()
+    let palindrome = true
+    for (let i in revarr) {
+        if (arr[i] != revarr[i]) {
+            palindrome = false
+            break
+        }
+    }
+    return palindrome
 };
+
+console.log(palindromes("rac3e3car"))
 
 // Do not edit below this line
 module.exports = palindromes;
